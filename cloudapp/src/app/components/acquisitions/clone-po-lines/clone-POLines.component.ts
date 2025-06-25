e7c45efa162b134e0de123a8e9ab1b7817ad3dd5
Pau Masot Agné <124433601+pmasotagne@users.noreply.github.com>
2025-06-25 11:32:05 +0200

    Update clone-POLines.component.ts
    Improvement in the visibility of the results and incorporation of a form for editing the POLines at the moment of cloning.

--- cloudapp/src/app/components/acquisitions/clone-po-lines/clone-POLines.component.ts
+++ cloudapp/src/app/components/acquisitions/clone-po-lines/clone-POLines.component.ts
@@ -3,7 +3,7 @@ import { AcquisitionService } from '../../../services/acquisition.service';
 import { Router } from '@angular/router';
 import { Entity } from '@exlibris/exl-cloudapp-angular-lib';
 import { of, forkJoin } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { formatDate } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { ProcessSummaryModalComponent } from '../../resume/resume.component';
@@ -12,6 +12,13 @@ import { UserAccessService } from '../../../services/userAccess.service';
 import { AlertService } from '@exlibris/exl-cloudapp-angular-lib';
 import { TranslateService } from '@ngx-translate/core';
 import { ProcessDataService } from '../../../services/process-data.service';
+import { fieldOptions, FieldOption } from './field-options';
+
+interface ClonedResult {
+  originalId: string;
+  newId?: string;
+  error?: string;
+}
 
 @Component({
   selector: 'clone-POLines',
@@ -29,6 +36,12 @@ export class ClonePOLinesComponent implements OnInit {
   processedCount : number;
   updatedCount : number;
   errorsCount : number;
+  clonedResults: ClonedResult[] = [];
+  enableFieldOverrides: boolean = false;
+
+  fieldOverrides: { field: string; value: string }[] = [
+    { field: '', value: '' }
+  ];
 
   private timer = new Timer();
 
@@ -37,6 +50,9 @@ export class ClonePOLinesComponent implements OnInit {
   // User Validation
   checkingUser: boolean = false;
   isModuleUserAllowed: boolean = false;
+
+   // Options
+  fieldOptions = fieldOptions;
 
   constructor(
     private acquisitionService: AcquisitionService,
@@ -67,7 +83,53 @@ export class ClonePOLinesComponent implements OnInit {
       );
 
     this.validateFirstEntity();
+    this.sortAndTranslateFieldOptions();
   }
+
+  private sortAndTranslateFieldOptions(): void {
+    const translationKeys = this.fieldOptions.map(option => option.label);
+  
+    this.translate.get(translationKeys).subscribe(translations => {
+      this.fieldOptions.forEach(option => {
+        option.label = translations[option.label];
+      });
+  
+      this.fieldOptions.sort((a, b) => a.label.localeCompare(b.label));
+    });
+  }
+
+  getAvailableOptions(currentIndex: number): FieldOption[] {
+    const selectedFields = this.fieldOverrides
+      .filter((_, i) => i !== currentIndex)
+      .map(f => f.field);
+    return fieldOptions.filter(opt => !selectedFields.includes(opt.value));
+  }
+
+  addField(): void {
+    if (this.hasAvailableOptions()) {
+      this.fieldOverrides.push({ field: '', value: '' });
+    }
+  }
+
+  removeField(index: number): void {
+    this.fieldOverrides.splice(index, 1);
+  }
+
+  hasAvailableOptions(): boolean {
+    const selectedFields = this.fieldOverrides.map(f => f.field);
+    return fieldOptions.some(opt => !selectedFields.includes(opt.value));
+  }
+
+  applyOverridesToPOLine(poLine: any): any {
+    if (!this.enableFieldOverrides) return poLine;
+
+    this.fieldOverrides.forEach(({ field, value }) => {
+      if (field) poLine[field] = value;
+    });
+
+    return poLine;
+  }
+
 
   back(): void {
     if (!this.loading) {
@@ -79,6 +141,7 @@ export class ClonePOLinesComponent implements OnInit {
     // Reset the arrays to allow a new selection and cloning cycle
     this.createdNumbers = [];
     this.selectedEntities = [];
+    this.clonedResults = [];
   }
 
   cloneSelectedEntities() {
@@ -88,6 +151,7 @@ export class ClonePOLinesComponent implements OnInit {
     this.totalRows = this.selectedEntities.length;
     //console.log('Selected Entities:', this.selectedEntities);
     this.loading = true;
+    this.clonedResults = [];
 
     this.timer.startTimer();
 
@@ -100,9 +164,15 @@ export class ClonePOLinesComponent implements OnInit {
       next: (responses) => {
         const creationObservables = [];
   
-        responses.forEach(poLine => {
+        responses.forEach((poLine, index) => {
+          //const originalId = this.selectedEntities[index].link;
+          const rawId = this.selectedEntities[index].link;
+          const lastPart = rawId.split('/').pop() || '';
+          const originalId = lastPart.startsWith('POL-') ? lastPart : `POL-${lastPart}`;
+
           if (poLine) {
             this.processedCount += 1;
+
             delete poLine.number;
             delete poLine.po_number;
             delete poLine.status;
@@ -110,56 +180,61 @@ export class ClonePOLinesComponent implements OnInit {
             const today = formatDate(new Date(), 'yyyy-MM-dd\'Z\'', 'en-US');
             poLine.status_date = today;
             poLine.created_date = today;
-  
-            // Build the observable for creating the modified PO Line, with error handling
+            poLine = this.applyOverridesToPOLine(poLine);
+
             const creationObservable = this.acquisitionService.createPOLine(poLine).pipe(
+              map(response => ({
+                originalId,
+                newId: response?.number,
+              })),
               catchError(err => {
                 this.errorsCount += 1;
-                // Return an empty object or null for failed creations
-                return of(null);
+                return of({
+                  originalId,
+                  error: err?.message || 'Unknown error'
+                });
               })
             );
-  
+
             creationObservables.push(creationObservable);
+          } else {
+            // poLine is null or undefined
+            this.clonedResults.push({
+              originalId,
+              error: 'Could not retrieve PO Line'
+            });
           }
         });
   
         // Now create all the modified PO Lines in parallel
         forkJoin(creationObservables).subscribe({
-          next: (creationResponses) => {  
-            // Filter successful PO Lines (those that are not null)
-            this.createdNumbers = creationResponses
-              .filter(response => response && (response as any).number)  // Only include non-null responses
-              .map(response => {
-                const number = (response as any).number;
-        
-                // Increment updatedCount for successful PO Lines
-                this.updatedCount += 1;
-        
-                // Return the number to be included in createdNumbers
-                return number;
-              });
-        
+          next: (results: ClonedResult[]) => {
+            results.forEach(result => {
+              if (result.newId) this.updatedCount += 1;
+            });
+
+            this.clonedResults = [...this.clonedResults, ...results];
+            this.timer.stopTimer();
             this.loading = false;
             this.openSummaryModal();
           },
           error: (err) => {
+            this.timer.stopTimer();
             this.loading = false;
           }
         });
       },
       error: (err) => {
+        this.timer.stopTimer();
         this.loading = false;
       }
     });
-
-    this.timer.stopTimer();
 
   }
 
   validateFirstEntity() {
     this.error = false;
-    console.log(this.selectedEntities[0]);
+    //console.log(this.selectedEntities[0]);
     if (this.selectedEntities.length > 0 && this.selectedEntities[0].type !== 'PO_LINE') {
       this.selectedEntities.length = 0;
       this.error = true;
@@ -200,6 +275,14 @@ export class ClonePOLinesComponent implements OnInit {
       }
     );
 
+  }
+
+  get successfulClones() {
+    return this.clonedResults.filter(result => result.newId);
+  }
+
+  get failedClones() {
+    return this.clonedResults.filter(result => result.error);
   }
 
 }
