29c56a36d6edee7f39f06bd073a291c8bc18f602
Pau Masot Agné <124433601+pmasotagne@users.noreply.github.com>
2025-08-20 11:16:51 +0200

    Update resume.component.ts
    Added MatExpansionModule for displaying errors in the results interface of a process.

--- cloudapp/src/app/components/resume/resume.component.ts
+++ cloudapp/src/app/components/resume/resume.component.ts
@@ -1,12 +1,20 @@
 import { Component, Inject } from "@angular/core"
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog"
-
+import { NgModule } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss']
 })
+
+@NgModule({
+  imports: [
+    MatExpansionModule
+  ]
+})
+  
 export class ProcessSummaryModalComponent {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
